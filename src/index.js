0c6e24fe1113751e95fffc0e1a00905721ee0255
Jacob Logan <jacoblogan98@gmail.com>
2022-03-18 17:21:33 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector("form");
+  const input = form.querySelector('#new-task-description');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildList(input.value);
+    form.reset();
+
+  function buildList(todo) {
+    let p = document.createElement('p');
+    let deleteButton = document.createElement('button');
+    deleteButton.addEventListener('click', handleDelete)
+    deleteButton.textContent = ' x';
+    p.textContent = `${todo} `;
+    p.appendChild(deleteButton)
+    document.querySelector('#list').appendChild(p);
+  }
+
+  function handleDelete(e) {
+    e.target.parentNode.remove();
+  }
+  })
 });
